e223103cee16cc612e9d4aa05e903e3cacddfdac
Lesibana Ngoepe <lesiban56@gmail.com>
2026-02-10 13:10:56 +0200

    Intial Commit

---
+++ src/main/java/com/example/CusTomBatchBackend/CusTomBatchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.CusTomBatchBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CusTomBatchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CusTomBatchBackendApplication.class, args);
+	}
+
+}
