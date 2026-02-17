acbf0ccfa2b2ceddc35d15e9572072afebd24180
Lesibana Ngoepe <lesiban56@gmail.com>
2026-02-17 13:05:10 +0200

    Add CategoryDTO class for category data transfer and management

---
+++ src/main/java/com/example/CusTomBatchBackend/dtos/CategoryDTO.java
@@ -0,0 +1,31 @@
+package com.example.CusTomBatchBackend.dtos;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CategoryDTO {
+
+
+    private Long id;
+
+    @NotBlank(message = "Name Required")
+    private String name;
+
+    private List<ProductDTO> products;
+
+}
