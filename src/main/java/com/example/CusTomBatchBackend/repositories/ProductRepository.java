4830208b85465de8bf0241f8f5a67280acd75324
Lesibana Ngoepe <lesiban56@gmail.com>
2026-02-20 13:19:27 +0200

    Add ProductRepository interface for product data management

--- src/main/java/com/example/CusTomBatchBackend/repositories/ProductRepository.java
+++ src/main/java/com/example/CusTomBatchBackend/repositories/ProductRepository.java
@@ -1,2 +1,14 @@
-package com.example.CusTomBatchBackend.repositories;public interface ProductRepository {
+package com.example.CusTomBatchBackend.repositories;
+
+import com.example.CusTomBatchBackend.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
+
+    List<Product> findByNameContainingOrDescriptionContaining(String name, String description);
+
+
+
 }
