47d4fa567dff85312651c2313370d0429478865d
Lesibana Ngoepe <lesiban56@gmail.com>
2026-02-23 23:30:15 +0200

    Add GlobalExceptionHandler for centralized exception handling

---
+++ src/main/java/com/example/CusTomBatchBackend/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,54 @@
+package com.example.CusTomBatchBackend.exceptions;
+
+
+import com.example.CusTomBatchBackend.dtos.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Response> handleAllExceptions(Exception ex) {
+        Response response = Response.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("An unexpected error occurred: " + ex.getMessage())
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Response> handleNotFoundException(NotFoundException ex) {
+        Response response = Response.builder()
+                .status(HttpStatus.NOT_FOUND.value())
+                .message("An unexpected error occurred: " + ex.getMessage())
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Response> handleNameValueRequiredException(NameValueRequiredException ex) {
+        Response response = Response.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message("An unexpected error occurred: " + ex.getMessage())
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Response> handleInvalidCredentialsException(InvalidCredentialsException ex) {
+        Response response = Response.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message("An unexpected error occurred: " + ex.getMessage())
+                .build();
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+
+}
